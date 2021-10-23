8d6cb9559779730c2d7449f20db0720b1061d1ac
Sean McFarlane <seanmcfarlane115@gmail.com>
2021-10-23 16:27:09 -0700

    Fixed oversized spline, and starting work on spline following train

---
+++ BCI_Unity/BCI Sandbox/Assets/SplineFollower.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineFollower : MonoBehaviour {
+
+	[SerializeField] private Spline targetSpline;
+
+	[SerializeField] private float distanceAlongSpline;
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}

55ba67d665bf5e4919f0ebf22deb393293ed40ff
Sean McFarlane <seanmcfarlane115@gmail.com>
2021-10-24 11:36:48 -0700

    Added working throttle indicator

--- BCI_Unity/BCI Sandbox/Assets/TrainEngine.cs
+++ BCI_Unity/BCI Sandbox/Assets/TrainEngine.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TrainEngine : MonoBehaviour {
 	bool initialized = false;
 	[SerializeField] [ReadOnly] SplineFollower mySplineFollower;
+	[SerializeField] [ReadOnly] Animator throttleAnimator;
 	[SerializeField] private float movementSpeed = 0;
 
 	[SerializeField] private float[] throttleLevels = { -1, -0.5f, 0, 0.5f, 1 };
@@ -24,6 +25,9 @@ public class TrainEngine : MonoBehaviour {
 
 	private void Init() {
 		if(initialized) return;
+		//TrainSprite/ThrottleIndicator
+		Transform throttleTransform = transform.Find("TrainSprite/ThrottleIndicator");
+		throttleAnimator = throttleTransform.GetComponent<Animator>();
 		mySplineFollower = this.GetComponent<SplineFollower>();
 		initialized = true;
 	}
@@ -32,10 +36,18 @@ public class TrainEngine : MonoBehaviour {
 		Init();
 	}
 
+	private void Update() {
+		throttleAnimator.speed = 0f;
+		//Lazy way to select a visual state - stop the animation and set the desired keyframe directly. TODO do a proper state machine in the future.
+		float animationFrameTime = 0.0999f+(float)currentThrottleLevel/(float)throttleLevels.Length;
+		Debug.Log(animationFrameTime);
+		throttleAnimator.Play("TrainThrottle", 0, animationFrameTime);
+	}
+
 	// Update is called once per frame
 	void FixedUpdate() {
 		movementSpeed += throttleLevels[currentThrottleLevel]*Time.fixedDeltaTime;
 		mySplineFollower.distanceAlongSpline += movementSpeed*Time.fixedDeltaTime;
-		Mathf.Clamp(movementSpeed, 0, 100f);
+		movementSpeed = Mathf.Clamp(movementSpeed, 0, 100f);
 	}
 }

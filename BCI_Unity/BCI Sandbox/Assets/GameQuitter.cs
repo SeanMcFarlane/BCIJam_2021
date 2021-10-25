f30266f6f2675d50438aeb220539705f09f6ed74
Sean McFarlane <seanmcfarlane115@gmail.com>
2021-10-24 22:38:12 -0700

    Build library path fix and other build related changes

---
+++ BCI_Unity/BCI Sandbox/Assets/GameQuitter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameQuitter : MonoBehaviour {
+	// Update is called once per frame
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
